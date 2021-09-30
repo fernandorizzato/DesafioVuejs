3e87fcd124f3f2a1c35a20921ddfdb91be864c9d
FernandoNunesRizzato <fernandorizzato@outlook.com>
2021-09-30 18:46:56 -0300

    rota

--- campeonato-brasileiro/src/App.vue
+++ campeonato-brasileiro/src/App.vue
@@ -1,55 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar color="green lighten-1" app dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
+    <AppTopBar />
     <v-main>
       <v-container></v-container>
-      <Inicio/>
+      <Inicio />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Inicio from "./components/Inicio.vue";
+import Inicio from "./views/Inicio.vue";
+import AppTopBar from "./components/shared/AppTopBar.vue";
 
 export default {
   name: "App",
 
   components: {
     Inicio,
+    AppTopBar,
   },
-
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
 };
 </script>
 
